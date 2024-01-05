56a3f3f000a0dbd4e01f5807b5c91ce488137c45
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-01-05 10:51:04 +0900

    Lock handles

--- src/YetAnotherHttpHandler/RequestContext.cs
+++ src/YetAnotherHttpHandler/RequestContext.cs
@@ -15,6 +15,7 @@ namespace Cysharp.Net.Http
         private readonly Pipe _pipe = new Pipe(System.IO.Pipelines.PipeOptions.Default);
         private readonly CancellationTokenSource _cancellationTokenSource;
         private readonly int _requestSequence;
+        private readonly object _handleLock = new object();
         private GCHandle _handle;
 
         internal YahaContextSafeHandle _ctxHandle;
@@ -47,18 +48,22 @@ namespace Cysharp.Net.Http
         public void Allocate()
         {
             Debug.Assert(!_handle.IsAllocated);
-
-            _handle = GCHandle.Alloc(this);
-            if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] State allocated");
+            lock (_handleLock)
+            {
+                _handle = GCHandle.Alloc(this);
+                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] State allocated");
+            }
         }
 
         public void Release()
         {
             Debug.Assert(_handle.IsAllocated);
-
-            if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Releasing state");
-            _handle.Free();
-            _handle = default;
+            lock (_handleLock)
+            {
+                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Releasing state");
+                _handle.Free();
+                _handle = default;
+            }
         }
 
         public static RequestContext FromHandle(IntPtr handle)
@@ -124,96 +129,113 @@ namespace Cysharp.Net.Http
 
         private unsafe void WriteBody(Span<byte> data)
         {
-            Debug.Assert(_handle.IsAllocated);
-
-            if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Sending the request body: Length={data.Length}");
-
-            // DangerousAddRef/Release are already called by caller.
-            var ctx = _ctxHandle.DangerousGet();
-            var requestContext = _requestContextHandle.DangerousGet();
-
-            while (!_requestBodyCompleted)
+            lock (_handleLock)
             {
-                // If the internal buffer is full, yaha_request_write_body returns false. We need to wait until ready to send bytes again.
-                if (NativeMethods.yaha_request_write_body(ctx, requestContext, (byte*)Unsafe.AsPointer(ref data.GetPinnableReference()), (UIntPtr)data.Length))
-                {
-                    break;
-                }
-                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Send buffer is full.");
+                Debug.Assert(_handle.IsAllocated);
 
-                // TODO:
-                Thread.Sleep(10);
+                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Sending the request body: Length={data.Length}");
+
+                // DangerousAddRef/Release are already called by caller.
+                var ctx = _ctxHandle.DangerousGet();
+                var requestContext = _requestContextHandle.DangerousGet();
+
+                while (!_requestBodyCompleted)
+                {
+                    // If the internal buffer is full, yaha_request_write_body returns false. We need to wait until ready to send bytes again.
+                    if (NativeMethods.yaha_request_write_body(ctx, requestContext, (byte*)Unsafe.AsPointer(ref data.GetPinnableReference()), (UIntPtr)data.Length))
+                    {
+                        break;
+                    }
+                    if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Send buffer is full.");
+
+                    // TODO:
+                    Thread.Sleep(10);
+                }
             }
         }
 
         private unsafe void TryCompleteBody(Exception? exception = default)
         {
-            Debug.Assert(_handle.IsAllocated);
-
-            if (_ctxHandle.IsClosed || _requestContextHandle.IsClosed)
+            lock (_handleLock)
             {
-                // The request has already completed. We need to nothing to do at here.
-                return;
-            }
-
-            if (_requestBodyCompleted)
-            {
-                return;
-            }
-
-            if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Info($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Complete sending the request body{(exception is null ? string.Empty : "; Exception=" + exception.Message)}");
-
-            var addRefContext = false;
-            var addRefReqContext = false;
-            try
-            {
-                _ctxHandle.DangerousAddRef(ref addRefContext);
-                _requestContextHandle.DangerousAddRef(ref addRefReqContext);
-
-                var ctx = _ctxHandle.DangerousGet();
-                var requestContext = _requestContextHandle.DangerousGet();
-
-                ThrowHelper.ThrowIfFailed(NativeMethods.yaha_request_complete_body(ctx, requestContext));
-            }
-            finally
-            {
-                if (addRefContext)
+                if (!_handle.IsAllocated)
                 {
-                    _ctxHandle.DangerousRelease();
+                    // The request has already completed. (`on_complete` callback has been called).
+                    return;
                 }
-                if (addRefReqContext)
-                {
-                    _requestContextHandle.DangerousRelease();
-                }
-            }
-            
-            _pipe.Reader.Complete(exception);
 
-            _requestBodyCompleted = true;
+                if (_ctxHandle.IsClosed || _requestContextHandle.IsClosed)
+                {
+                    // The request has already completed. We need to nothing to do at here.
+                    return;
+                }
+
+                if (_requestBodyCompleted)
+                {
+                    return;
+                }
+
+                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Info($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Complete sending the request body{(exception is null ? string.Empty : "; Exception=" + exception.Message)}");
+
+                var addRefContext = false;
+                var addRefReqContext = false;
+                try
+                {
+                    _ctxHandle.DangerousAddRef(ref addRefContext);
+                    _requestContextHandle.DangerousAddRef(ref addRefReqContext);
+
+                    var ctx = _ctxHandle.DangerousGet();
+                    var requestContext = _requestContextHandle.DangerousGet();
+
+                    ThrowHelper.ThrowIfFailed(NativeMethods.yaha_request_complete_body(ctx, requestContext));
+                }
+                finally
+                {
+                    if (addRefContext)
+                    {
+                        _ctxHandle.DangerousRelease();
+                    }
+                    if (addRefReqContext)
+                    {
+                        _requestContextHandle.DangerousRelease();
+                    }
+                }
+                
+                _pipe.Reader.Complete(exception);
+
+                _requestBodyCompleted = true;
+            }
         }
 
         public unsafe bool TryAbort()
         {
-            Debug.Assert(_handle.IsAllocated);
-            if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Try to abort the request");
-
-            _cancellationTokenSource.Cancel();
-
-            var addRefContextHandle = false;
-            var addRefRequestContextHandle = false;
-            try
+            lock (_handleLock)
             {
-                _ctxHandle.DangerousAddRef(ref addRefContextHandle);
-                _requestContextHandle.DangerousAddRef(ref addRefRequestContextHandle);
+                if (YahaEventSource.Log.IsEnabled()) YahaEventSource.Log.Trace($"[ReqSeq:{_requestSequence}:State:0x{Handle:X}] Try to abort the request");
+                if (!_handle.IsAllocated)
+                {
+                    // If the handle has been already released, the request is fully completed.
+                    return false;
+                }
 
-                NativeMethods.yaha_request_abort(_ctxHandle.DangerousGet(), _requestContextHandle.DangerousGet());
+                _cancellationTokenSource.Cancel();
+
+                var addRefContextHandle = false;
+                var addRefRequestContextHandle = false;
+                try
+                {
+                    _ctxHandle.DangerousAddRef(ref addRefContextHandle);
+                    _requestContextHandle.DangerousAddRef(ref addRefRequestContextHandle);
+
+                    NativeMethods.yaha_request_abort(_ctxHandle.DangerousGet(), _requestContextHandle.DangerousGet());
+                }
+                finally
+                {
+                    _ctxHandle.DangerousRelease();
+                    _requestContextHandle.DangerousRelease();
+                }
+                return true;
             }
-            finally
-            {
-                _ctxHandle.DangerousRelease();
-                _requestContextHandle.DangerousRelease();
-            }
-            return true;
         }
 
         public void Dispose()
