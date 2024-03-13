c75983cc69381fb46a97e4cd7762385f14ed554b
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-03-13 17:22:48 +0900

    Create unitypackages using NuGetForUnity

--- src/YetAnotherHttpHandler.Unity/Assets/Editor/Scripts/PackageExporter.cs
+++ src/YetAnotherHttpHandler.Unity/Assets/Editor/Scripts/PackageExporter.cs
@@ -4,69 +4,87 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NugetForUnity;
+using NugetForUnity.Configuration;
+using NugetForUnity.Models;
 using UnityEditor;
-using UnityEditor.PackageManager.Requests;
+using UnityEngine;
 
 public static class PackageExporter
 {
-    private static ListRequest s_listRequest;
-
     [MenuItem("Tools/Export Unitypackage")]
     public static void Export()
     {
         EditorUtility.DisplayProgressBar("Packges", "Exporting packages, please wait...", 0);
 
-        s_listRequest = UnityEditor.PackageManager.Client.List(offlineMode: false, includeIndirectDependencies: true);
-
-        EditorApplication.update += OnEditorUpdate;
+        var packagesYahaDependencies = new[] { "System.IO.Pipelines", "System.Runtime.CompilerServices.Unsafe" };
+        var packagesGrpcNetClient = new[] { "Grpc.Net.Client" };
+        ExportNuGetPackage(packagesYahaDependencies, "Cysharp.Net.Http.YetAnotherHttpHandler.Dependencies", Array.Empty<string>());
+        ExportNuGetPackage(packagesGrpcNetClient, "Grpc.Net.Client.Dependencies", packagesYahaDependencies);
     }
 
-    private static void OnEditorUpdate()
+    private static void ExportNuGetPackage(IReadOnlyList<string> packageIds, string unityPackageName, IReadOnlyList<string> excludePackageIds)
     {
-        if (s_listRequest == null || !s_listRequest.IsCompleted)
-        {
-            return;
-        }
-
-        EditorApplication.update -= OnEditorUpdate;
-
-        ExportPackage("com.cysharp.yetanotherhttphandler.dependencies", "Cysharp.Net.Http.YetAnotherHttpHandler.Dependencies", includeSelf: false);
-        ExportPackage("org.nuget.grpc.net.client", "Grpc.Net.Client.Dependencies", includeSelf: true);
-
-        EditorUtility.ClearProgressBar();
-    }
-
-    private static void ExportPackage(string packageName, string unityPackageName, bool includeSelf)
-    {
-        List<UnityEditor.PackageManager.PackageInfo> dependencies = new();
-
-        UnityEditor.PackageManager.PackageInfo packageInfo = s_listRequest.Result.First(p => p.name.Equals(packageName, StringComparison.Ordinal));
-
-        if (includeSelf)
-        {
-            dependencies.Add(packageInfo);
-        }
-
-        GetAllDependencies(packageInfo, dependencies);
-
         string exportPath = $"./{unityPackageName}.unitypackage";
 
-        AssetDatabase.ExportPackage(dependencies.Select(x => x.assetPath).ToArray(), exportPath, ExportPackageOptions.Recurse);
-
-        UnityEngine.Debug.Log($"Export complete: {Path.GetFullPath(exportPath)}");
-    }
-
-    private static void GetAllDependencies(UnityEditor.PackageManager.PackageInfo packageInfo, ICollection<UnityEditor.PackageManager.PackageInfo> dependencies)
-    {
-        foreach (UnityEditor.PackageManager.DependencyInfo dependency in packageInfo.dependencies)
+        var packages = new HashSet<INugetPackageIdentifier>();
+        var installedPackages = InstalledPackagesManager.InstalledPackages.ToArray();
+        foreach (var package in installedPackages.Where(x => packageIds.Contains(x.Id)))
         {
-            UnityEditor.PackageManager.PackageInfo resolvedPackageInfo = s_listRequest.Result.First(p => p.name.Equals(dependency.name, StringComparison.Ordinal));
+            foreach (var dep in TraverseDependencies(installedPackages, package, includesSelf: true))
+            {
+                if (excludePackageIds.Contains(dep.Id))
+                {
+                    continue;
+                }
+                packages.Add(dep);
+            }
+        }
 
-            dependencies.Add(resolvedPackageInfo);
+        var assetPaths = packages
+            .Select(x =>
+            {
+                var directory = GetPackageInstallDirectory(x);
+                return "Assets" + directory.Substring(Application.dataPath.Length);
+            })
+            .ToArray();
 
-            GetAllDependencies(resolvedPackageInfo, dependencies);
+        Debug.Log($"Exporting: {Path.GetFullPath(exportPath)}");
+        foreach (var package in assetPaths)
+        {
+            Debug.Log(package);
+        }
+
+        AssetDatabase.ExportPackage(assetPaths, exportPath, ExportPackageOptions.Recurse);
+
+        Debug.Log($"Export complete");
+
+        static IEnumerable<INugetPackage> TraverseDependencies(IReadOnlyList<INugetPackage> installedPackages, INugetPackage package, bool includesSelf)
+        {
+            if (includesSelf)
+            {
+                yield return package;
+            }
+
+            foreach (var dep in package.CurrentFrameworkDependencies)
+            {
+                var depPackage = installedPackages.FirstOrDefault(x => x.Id == dep.Id);
+                if (depPackage != null)
+                {
+                    yield return depPackage;
+
+                    foreach (var depDep in TraverseDependencies(installedPackages, depPackage, includesSelf: false))
+                    {
+                        yield return depDep;
+                    }
+                }
+            }
         }
     }
-}
 
+    private static string GetPackageInstallDirectory(INugetPackageIdentifier package)
+    {
+        return Path.Combine(ConfigurationManager.NugetConfigFile.RepositoryPath, $"{package.Id}.{package.Version}");
+    }
+}
 #endif
