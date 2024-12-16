a455fb2414f503f78b10baf2efcf3ae639b86ac8
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-12-16 16:06:29 +0900

    Add negotiation tests

--- test/YetAnotherHttpHandler.Test/Http2Test.cs
+++ test/YetAnotherHttpHandler.Test/Http2Test.cs
@@ -1,6 +1,4 @@
-using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Cysharp.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
