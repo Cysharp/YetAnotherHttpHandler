2521356555340c88f81a60327e080c5bc0db3474
Mayuki Sawatari <mayuki+github@misuzilla.org>
2024-12-19 14:58:23 +0900

    Add unit tests

---
+++ test/YetAnotherHttpHandler.Test/UdsTest.cs
@@ -0,0 +1,41 @@
+using System.Security.Cryptography.X509Certificates;
+using Cysharp.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Xunit.Abstractions;
+
+namespace _YetAnotherHttpHandler.Test;
+
+public class UdsTest(ITestOutputHelper testOutputHelper) : Http2TestBase(testOutputHelper)
+{
+    private readonly string _udsPath = Path.Combine(Path.GetTempPath(), $"yaha-uds-test-{Guid.NewGuid()}");
+    protected override YetAnotherHttpHandler CreateHandler()
+    {
+        return new YetAnotherHttpHandler()
+        {
+            UnixDomainSocketPath = _udsPath,
+            Http2Only = true,
+        };
+    }
+
+    protected override Task<TestWebAppServer> LaunchServerAsyncCore<T>(Action<WebApplicationBuilder>? configure = null)
+    {
+        return LaunchServerAsync<T>(TestWebAppServerListenMode.SecureHttp2Only, builder =>
+        {
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenUnixSocket(_udsPath, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http2;
+                });
+                
+                // hyperlocal uses the 'unix' scheme and passes the URI to hyper. As a result, the ':scheme' header in the request is set to 'unix'.
+                // By default, Kestrel does not accept non-HTTP schemes. To allow non-HTTP schemes, we need to set 'AllowAlternateSchemes' to true.
+                options.AllowAlternateSchemes = true;  
+            });
+
+            configure?.Invoke(builder);
+        });
+    }
+}
